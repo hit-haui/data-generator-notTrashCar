14bc47fd07e85bf53770e95865fdd87fbcdeb45a
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-02-22 10:47:20 +0700

    Multiple subscriber with new topic

--- src/team705/src/main.py
+++ src/team705/src/main.py
@@ -6,6 +6,7 @@ import time
 import cv2
 import numpy as np
 
+import message_filters
 import rospkg
 import rospy
 from sensor_msgs.msg import CompressedImage
@@ -28,39 +29,38 @@ def car_control(angle, speed):
     '''
     Hàm này dùng để gửi tín hiệu đến simulator
     '''
-    pub_speed = rospy.Publisher('/team705_speed', Float32, queue_size=10)
+    pub_speed = rospy.Publisher('/set_speed_car_api', Float32, queue_size=10)
     pub_speed.publish(speed)
-    pub_angle = rospy.Publisher('/team705_steerAngle', Float32, queue_size=10)
+    pub_angle = rospy.Publisher('/set_steer_car_api', Float32, queue_size=10)
     pub_angle.publish(angle)
     print('Angle:', angle, 'Speed:', speed)
 
 
-def image_callback(data):
+def image_callback(rgb_data, depth_data):
     '''
-    Hàm này được gọi mỗi khi simulator trả về một bức ảnh, vậy nên có thể gọi điều khiển xe ở đây
+    Hàm này được gọi mỗi khi simulator trả về ảnh, vậy nên có thể gọi điều khiển xe ở đây
     '''
     start_time = time.time()
-    temp = np.fromstring(data.data, np.uint8)
-    img = cv2.imdecode(temp, cv2.IMREAD_COLOR)
-    cv2.imshow('raw_frame', img)
-    # video_out.write(img)
+    temp = np.fromstring(rgb_data.data, np.uint8)
+    rgb_img = cv2.imdecode(temp, cv2.IMREAD_COLOR)
+    temp = np.fromstring(depth_data.data, np.uint8)
+    depth_img = cv2.imdecode(temp, cv2.IMREAD_COLOR)
+    cv2.imshow('rgb_frame', rgb_img)
+    cv2.imshow('depth_frame', depth_img)
     cv2.waitKey(1)
+    # video_out.write(img)
     print('FPS:', 1/(time.time() - start_time))
-
-
-'''
-Nên viết thêm một hàm để nhận tín hiệu từ bàn phím và chuyển hoá thành góc quay và tốc độ, call nó trong `image_callback`,
-để ghi nhận được 1 chuỗi phím thì nên dùng biến toàn cục để lưu lại, sao cho khi đang ở trong callback cũng có thể truy cập được.
-Nhận phím thế nào thì chịu khó google nhé.
-'''
 
 
 def main():
     rospy.init_node('team705_node', anonymous=True)
-    rospy.Subscriber("/team705_image/compressed", CompressedImage,
-                     image_callback, queue_size=1, buff_size=2**24)
+    rgb_sub = message_filters.Subscriber(
+        '/camera/rgb/image_raw', CompressedImage, buff_size=2**24)
+    depth_sub = message_filters.Subscriber(
+        '/camera/depth/image_raw', CompressedImage, buff_size=2**24)
+    ts = message_filters.TimeSynchronizer([rgb_sub, depth_sub], queue_size=1)
+    ts.registerCallback(image_callback)
     rospy.spin()
-
     # video_out.release()
     # print('Saved', video_out_name)
 
