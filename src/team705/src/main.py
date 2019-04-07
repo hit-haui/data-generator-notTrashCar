5116743127a8ac2c342772f691962f6852bf64ad
linus <linus@cinnamon.is>
2019-04-07 18:04:55 +0700

    Init basic lane detection

--- src/team705/src/main.py
+++ src/team705/src/main.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
+import cv2
 import math
 import os
 import sys
 import time
-
-#import cv2
-import message_filters
 import numpy as np
 import rospkg
 import rospy
-import tensorflow as tf
-from keras.models import load_model
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Float32
+from lane_detect import *
+from param import *
+# from keras.backend.tensorflow_backend import set_session
+# config = tf.ConfigProto()
+# config.gpu_options.per_process_gpu_memory_fraction = 0.3
+# set_session(tf.Session(config=config))
 
-from keras.backend.tensorflow_backend import set_session
-config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.3
-set_session(tf.Session(config=config))
+# model_path = '/home/dejavu/read_data_2chanel-054-524.97807.hdf5'
+# img_size = (320, 240, 1)
+# model = load_model(model_path)
+# graph = tf.get_default_graph()
 
-model_path = '/home/dejavu/read_data_2chanel-054-524.97807.hdf5'
-img_size = (320, 240, 1)
-model = load_model(model_path)
-graph = tf.get_default_graph()
+# print('Loaded model')
 
-print('Loaded model')
-
-#try:
+# try:
 #    os.chdir(os.path.dirname(__file__))
 #    os.system('clear')
 #   print("\nWait for initial setup, please don't connect anything yet...\n")
-sys.path.remove('/opt/ros/lunar/lib/python2.7/dist-packages')
-#except:
-#    pass
+try:
+    sys.path.remove('/opt/ros/lunar/lib/python2.7/dist-packages')
+except:
+   pass
 
-import cv2
 
 def car_control(angle, speed):
     '''
@@ -47,111 +44,60 @@ def car_control(angle, speed):
     print('Angle:', angle, 'Speed:', speed)
 
 
-def get_prediction(img):
-    global model, graph
-    s = img.shape
-    img = img[:s[0]//2, :]
-    output = img.copy()
-    raw = output.copy()
+def process_frame(raw_img):
+    # Crop from sky line down
+    print(sky_line)
+    raw_img = raw_img[sky_line:, :]
+    print(raw_img.shape)
+    # Hide sensor and car's hood
+    # raw_img = cv2.rectangle(raw_img, top_left_proximity,
+    #                         bottom_right_proximity, hood_fill_color, -1)
+    # raw_img = cv2.rectangle(raw_img, top_left_hood,
+    #                         bottom_right_hood, hood_fill_color, -1)
+    cv2.imshow('raw', raw_img)
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # Handle shadow by using complex sobel operator
+    combined = get_combined_binary_thresholded_img(
+        cv2.cvtColor(raw_img, cv2.COLOR_BGR2RGB)) * 255
+    # combined = cv2.adaptiveThreshold(combined, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    #                             cv2.THRESH_BINARY, 51, 2)
+    # combined = cv2.bitwise_not(combined)
 
-    lower_blue = np.array([100, 70, 70])
-    upper_blue = np.array([140, 255, 255])
-
-    mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    res = cv2.bitwise_and(img, img, mask=mask)
-    color = res.copy()
-    res = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-    res = cv2.adaptiveThreshold(res, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                cv2.THRESH_BINARY, 17, 2)
-    # detect circles in the image
-    circles = cv2.HoughCircles(
-        res, cv2.HOUGH_GRADIENT, 1, 20, param1=100, param2=50)
-    left = 0
-    none = 0
-    right = 0
-    # ensure at least some circles were found
-    if circles is not None and np.sum(circles) > 0:
-        # convert the (x, y) coordinates and radius of the circles to integers
-        circles = np.round(circles[0, :]).astype("int")
-        # print('Got', len(circles), 'circles')
-
-        # loop over the (x, y) coordinates and radius of the circles
-        for index_phu, (x, y, r) in enumerate(circles):
-
-            #print(x, y, r)
-            # print(x, y, r)
-            # draw the circle in the output image, then draw a rectangle
-            # corresponding to the center of the circle
-            cv2.circle(output, (x, y), r, (0, 0, 255), 4)
-            top_y = max(y - r - 10, 0)
-            top_x = max(x - r - 10, 0)
-            y_size = min(top_y+r*2+20, img.shape[0])
-            x_size = min(top_x+r*2+20, img.shape[1])
-            #print(top_x, top_y)
-            #print(img.shape)
-            img = img[top_y:y_size, top_x:x_size, :]
-            h,w,c = img.shape
-            if h and w !=0:
-                if c != 1:
-                    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                img = cv2.resize(img,(80,80))
-                img = np.expand_dims(img,axis=-1)
-                with graph.as_default():
-                    traffic_list = model_traffic.predict(np.array([img]))[0]
-                # print('predict:',traffic_list)
-                l = traffic_list[0]
-                n = traffic_list[1]
-                r = traffic_list[2]
-                if max(l,max(n,r)) == traffic_list[0]:
-                    left +=1
-                elif max(l, max(n, r)) == traffic_list[1]:
-                    none +=1
-                elif max(l, max(n, r)) == traffic_list[2]:
-                    right +=1
-    if max(left,right) == left:
-        np_img = np.array([0,1,0])
-    elif max(left,right) == right:
-        np_img = np.array([0,0,1])
-    elif left and right == none:
-        np_img = np.array([0, 0, 0])
-    with graph.as_default():
-        angle = model.predict(np_img)[0][0] - 60
-    return angle
+    # Simple color filltering + Canny Edge detection
+    # combined = easy_lane_preprocess(raw_img)
+    line_image, angle = hough_lines(combined, rho, theta,
+                                    threshold, min_line_length, max_line_gap)
+    test_img = cv2.cvtColor(combined, cv2.COLOR_GRAY2RGB)
+    annotated_image = cv2.cvtColor(weighted_img(
+        line_image, test_img), cv2.COLOR_RGB2BGR)
+    return annotated_image, angle
 
 
 def image_callback(rgb_data):
     '''
     Hàm này được gọi mỗi khi simulator trả về ảnh, vậy nên có thể gọi điều khiển xe ở đây
     '''
-    global rgb_index, depth_index
     start_time = time.time()
     temp = np.fromstring(rgb_data.data, np.uint8)
     rgb_img = cv2.imdecode(temp, cv2.IMREAD_COLOR)
-    rgb_img = cv2.cvtColor(rgb_img,cv2.COLOR_BGR2GRAY)
-    rgb_img = cv2.resize(rgb_img, img_size[:-1]) 
-    #print(rgb_img.shape)
-    rgb_img = np.expand_dims(rgb_img,axis=3)
-    car_control(angle=get_prediction(rgb_img), speed=9)
-    print("FPS:",1/(time.time()-start_time))
+    # rgb_img = cv2.resize(rgb_img, (480, 640))
+    # print(rgb_img.shape)
+    annotated_image, angle = process_frame(rgb_img)
+    cv2.imshow('processed_frame', annotated_image)
+    cv2.waitKey(1)
+    # car_control(angle=angle, speed=25)
+    # rgb_img = cv2.resize(rgb_img, img_size[:-1])
+    print("FPS:", 1/(time.time()-start_time))
+    print('Angle:', angle)
+    print('-----------------------------------')
 
 def main():
     rospy.init_node('team705_node', anonymous=True)
-    rgb_sub = message_filters.Subscriber(
-        '/camera/rgb/image_raw/compressed/', CompressedImage, buff_size=2**32)
-    depth_sub = message_filters.Subscriber(
-       '/camera/depth/image_raw/compressed/', CompressedImage, buff_size=2**32)
-
-    ts = message_filters.ApproximateTimeSynchronizer(
-        rgb_sub, queue_size=1, slop=0.1)
-    ts.registerCallback(image_callback)
-    rgb_sub = rospy.Subscriber('/camera/rgb/image_raw/compressed/', 
-     CompressedImage, image_callback , buff_size=2**16, queue_size=1)
+    rospy.Subscriber(
+        '/camera/rgb/image_raw/compressed/', CompressedImage, buff_size=2**32, queue_size=1, callback=image_callback)
     try:
         rospy.spin()
     except KeyboardInterrupt:
-        car_control(angle = 0, speed = 0 )
         pass
 
 
